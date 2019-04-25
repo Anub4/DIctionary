14a19315d6413b87e9b42ad2afa2e095e69f6979
Softwarica College Lab <softwarica@softwarica.com>
2019-04-25 11:40:32 +0545

    Complete data persistence

---
+++ app/src/main/java/np/com/softwarica/dictionary/AddWordActivity.java
@@ -0,0 +1,52 @@
+package np.com.softwarica.dictionary;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class AddWordActivity extends AppCompatActivity {
+
+    private EditText etWord,etMeaning;
+    private Button btnWord;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_word);
+
+        etWord= findViewById(R.id.etWord);
+        etMeaning= findViewById(R.id.etMeaning);
+        btnWord= findViewById(R.id.btnWord);
+
+        btnWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Save();
+            }
+        });
+    }
+
+    private void Save() {
+        try {
+            PrintStream printStream = new PrintStream(openFileOutput("words.txt", MODE_PRIVATE | MODE_APPEND));
+            printStream.println(etWord.getText().toString() + "->" + etMeaning.getText().toString());
+            printStream.close();
+            Toast.makeText(this,"Saved to" + getFilesDir(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Log.d("Dictionary app", "Message error" + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+}
--- app/src/main/java/np/com/softwarica/dictionary/MainActivity.java
+++ app/src/main/java/np/com/softwarica/dictionary/MainActivity.java
@@ -6,51 +6,72 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String countires[] = {
-            " Nepal", "Kathamandu",
-            "India", "Delhi",
-            "China", "Beijing",
-            "UK", "London",
-            "US", "Washington DC",
-            "Canada", "Ottawa"
 
-    };
     private Map<String, String> dictionary;
+    private Button btnAddMoreWords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listcountries = findViewById(R.id.countryarray);
+        btnAddMoreWords= findViewById(R.id.btnAddMoreWords);
+
+        btnAddMoreWords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddWordActivity.class);
+                startActivity(intent);
+            }
+        });
         dictionary = new HashMap<>();
-        for (int i = 0;
-        i<countires.length;i+=2){
-            dictionary.put(countires[i],countires[i+1]);
-        }
-            ArrayAdapter countryAdapter =new ArrayAdapter<>(this,
-            android.R.layout.simple_list_item_1,
-                    new ArrayList(dictionary.keySet())
+
+        readFromFile();
+        ArrayAdapter countryAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList(dictionary.keySet())
         );
         listcountries.setAdapter(countryAdapter);
         listcountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String country=parent.getItemAtPosition(position).toString();
-                String capital=dictionary.get(country);
-                Intent intent= new Intent(MainActivity.this, CapitalActivity.class);
-                intent.putExtra("capital",capital );
+                String country = parent.getItemAtPosition(position).toString();
+                String capital = dictionary.get(country);
+                Intent intent = new Intent(MainActivity.this, CapitalActivity.class);
+                intent.putExtra("capital", capital);
                 startActivity(intent);
             }
         });
 
+    }
+
+    private void readFromFile() {
+        try {
+            FileInputStream fos = openFileInput("words.txt");
+            InputStreamReader isr = new InputStreamReader(fos);
+            BufferedReader br = new BufferedReader(isr);
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("->");
+                dictionary.put(parts[0], parts[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
